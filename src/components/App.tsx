1e425d34f4b444e2155ae8cc5f7da36b98bb1262
Julia Vamboldt <26203359+julvmdt@users.noreply.github.com>
2022-08-26 14:08:21 +0200

    add send comment component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,12 @@
 import Comment from "./Comment";
+import SendComment from "./SendComment";
 
 const App = () => {
     return (
-        <Comment avatarName="amyrobson" userName="amyrobson" commentCreated="1 month ago" comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well." />
+        <main>
+            <Comment avatarName="amyrobson" userName="amyrobson" commentCreated="1 month ago" comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well." />
+            <SendComment avatarName="amyrobson" />
+        </main>
     )
 }
 
--- src/components/SendComment.tsx
+++ src/components/SendComment.tsx
@@ -1,5 +1,21 @@
-const SendComment = () => {
+import "./SendComment.scss";
 
+type SendComment = {
+    avatarName: string;
+};
+
+const SendComment = ({ avatarName }: SendComment) => {
+    return (<section className="send-comment-container">
+        <label htmlFor="comment">Add a comment</label>
+        <textarea id="comment" className="send-comment-textarea" />
+        <footer className="send-comment-footer">
+            <img src={`${process.env.PUBLIC_URL}/avatars/image-${avatarName}.png`} alt="A avatar image of the user" className="send-comment-footer__avatar" />
+            <button className="send-comment-footer__button">
+                <span>Send</span>
+            </button>
+        </footer>
+    </section >
+    );
 }
 
 export default SendComment;
