cd410ceaf28a7d866880ae80879d1efe75d7812b
Julia Vamboldt <26203359+julvmdt@users.noreply.github.com>
2022-08-26 14:42:50 +0200

    add delete dialog component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Comment from "./Comment";
+import DeleteCommentDialog from "./DeleteCommentDialog";
 import SendComment from "./SendComment";
 
 const App = () => {
+    const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
     return (
         <main>
             <Comment avatarName="amyrobson" userName="amyrobson" commentCreated="1 month ago" comment="Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You’ve nailed the design and the responsiveness at various breakpoints works really well." />
             <SendComment avatarName="amyrobson" />
+            <button onClick={() => setShowDeleteDialog(true)}>Delete</button>
+            {showDeleteDialog && (
+                <DeleteCommentDialog />
+            )}
         </main>
-    )
+    );
 }
 
 export default App;
--- src/components/DeleteCommentDialog.tsx
+++ src/components/DeleteCommentDialog.tsx
@@ -1,5 +1,19 @@
-const DeleteCommentDialog = () => {
+import "./DeleteCommentDialog.scss";
 
+const DeleteCommentDialog = () => {
+    return (
+        <>
+            <div className="delete-dialog-backdrop" />
+            <section className="delete-dialog">
+                <h1>Delete comment</h1>
+                <p>Are you sure you want to delete this comment? This will remove the comment and can’t be undone.</p>
+                <section className="delete-dialog__button-group">
+                    <button className="delete-dialog__cancel-button">No, cancel</button>
+                    <button className="delete-dialog__delete-button">Yes, delete</button>
+                </section>
+            </section>
+        </>
+    );
 }
 
 export default DeleteCommentDialog;
